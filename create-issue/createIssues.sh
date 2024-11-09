2d79c894c30ffcea8e2b0b0e657444e80c9a059b
Basyrov Rustam <hrustbas@gmail.com>
2024-11-09 21:42:19 +0300

    shell script

--- create-issue/createIssues.sh
+++ create-issue/createIssues.sh
@@ -2,4 +2,16 @@
 
 # set -xe
 
-grep -rn "TODO:" | sed -E 's/(.*):[0-9]:.*TODO:\s?(.*)/\1|\2/'
+TASKS=$(mktemp)
+
+for f in $@
+do
+    grep -rn "TODO:" "$f" | sed -E 's/(.*):[0-9]:.*TODO:\s?(.*)/\1|\2/' >> $TASKS
+done
+
+while read task
+do
+    echo $task
+done < $TASKS
+
+rm $TASKS
