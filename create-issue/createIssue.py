0096f38d241841d3013fd5d83f2c919b88587e26
Basyrov Rustam <hrustbas@gmail.com>
2024-11-10 20:26:32 +0300

    created issue

--- create-issue/createIssue.py
+++ create-issue/createIssue.py
@@ -41,13 +41,40 @@ if r.status_code == 200:
     results = r.json()
     # print(results)
 
-    issues2create = []
+    issues = []
 
     for res in results:
-        if TODO != res['title']:
-            issues2create.append((TODO, FILE))
+        issues.append(res['title'])
 
-    print(issues2create)
+    print(issues)
+else:
+    print(r.status_code)
+    pprint(r.json())
+
+###################
+# CREATE AN ISSUE #
+###################
+
+if TODO in issues:
+    exit(1)
+
+
+headers = {
+        "Accept": "application/vnd.github+json" ,
+        "Authorization": f"Bearer {GITHUB_TOKEN}" ,
+        "X-GitHub-Api-Version": "2022-11-28" ,
+}
+
+data = {
+        'title': f'TODO: {TODO}',
+        'body':f'{FILE}',
+        'labels': ['TODO'],
+}
+
+r = requests.post(URL, headers=headers, json=data)
+
+if r.status_code == 201:
+    print(f"TODO: {TODO} -- Success!")
 else:
     print(r.status_code)
     pprint(r.json())
