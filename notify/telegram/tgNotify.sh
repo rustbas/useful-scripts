eb74a4262f043d8a6dbe4d3c15e5d8c8692d465c
Basyrov Rustam <hrustbas@gmail.com>
2025-06-04 21:01:00 +0300

    create notify func

--- notify/telegram/tgNotify.sh
+++ notify/telegram/tgNotify.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/bash
 
 notify() {
-    declare data="$(</dev/stdin)";
-    echo $data
+    declare raw_data="$(</dev/stdin)";
+    echo $raw_data
 }
 
 set -e
 
 ENV_FILE="/home/rustam/ws/projects/useful-scripts/notify/telegram/.env"
 
-. "$ENV_FILE"
+# . "$ENV_FILE"
 
-notify
+DATA=`notify`
+
+[ -z "$TG_TOKEN" ]  && echo "Provide a TG_TOKEN variable!"  1>&2 && exit 1
+[ -z "$CHAT_ID" ]   && echo "Provide a CHAT_ID variable!"   1>&2 && exit 1
+[ -z "$THREAD_ID" ] && echo "Provide a THREAD_ID variable!" 1>&2 && exit 1
+
+CURL="curl"
+CURL_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+CURL_MSG="""
+_Notification from command line_
+
+\`\`\`
+$DATA
+\`\`\`
+"""
+
+$CURL -X POST \
+      -s \
+      -o /dev/null \
+      -H 'Content-Type: application/json' \
+      -d "{
+            \"chat_id\": $CHAT_ID, \
+            \"message_thread_id\":\"$THREAD_ID\", \
+            \"text\": \"$CURL_MSG\", \
+            \"disable_notification\": true, \
+            \"parse_mode\":\"MarkdownV2\"}" \
+      $CURL_URL
